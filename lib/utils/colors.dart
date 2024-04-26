2ca5daa2855dde3007163cf9ad52630fe19e99e3
shrutikunwar <shruti.kunwar@apexcollege.edu.np>
2024-04-26 21:38:22 +0545

    again

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,24 +1,24 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// // const Color cGreen = Color(0xFF638d6a);
-// // const Color cRed = Color(0xFFff6363);
+// const Color cGreen = Color(0xFF638d6a);
+// const Color cRed = Color(0xFFff6363);
 
-// const Color cBlue = Color(0xFF0E86D4);
-// const Color cGreen = Color.fromARGB(255, 66, 133, 77);
-// const Color cRed = Color.fromARGB(255, 222, 45, 45);
-// const Color cWhite = Color(0xFFffffff);
-// const Color cBlack = Color(0xFF000000);
-// const Color cGrayOld = Color.fromARGB(255, 121, 119, 119);
-// const Color cOrangeOld = Color(0xFFFFA050);
+const Color cBlue = Color(0xFF0E86D4);
+const Color cGreen = Color.fromARGB(255, 66, 133, 77);
+const Color cRed = Color.fromARGB(255, 222, 45, 45);
+const Color cWhite = Color(0xFFffffff);
+const Color cBlack = Color(0xFF000000);
+const Color cGrayOld = Color.fromARGB(255, 121, 119, 119);
+const Color cOrangeOld = Color(0xFFFFA050);
 
-// //New
-// const Color cBrown = Color(0xFFED9F2A);
-// const Color cGolden = Color(0xFFF7C159);
-// const Color cOrange = Color(0xFFFA973B);
-// const Color cSilver = Color(0xFFF4F0F0);
-// const Color cGray = Color(0xFFEBE1E1);
+//New
+const Color cBrown = Color(0xFFED9F2A);
+const Color cGolden = Color(0xFFF7C159);
+const Color cOrange = Color(0xFFFA973B);
+const Color cSilver = Color(0xFFF4F0F0);
+const Color cGray = Color(0xFFEBE1E1);
 
-// // Trial
-// const Color cGLight = Color(0xFFF6E2A3);
-// const Color cGBrown = Color(0xFFBD8936);
-// const Color cGLBrown = Color(0xFFE0B35A);
+// Trial
+const Color cGLight = Color(0xFFF6E2A3);
+const Color cGBrown = Color(0xFFBD8936);
+const Color cGLBrown = Color(0xFFE0B35A);
