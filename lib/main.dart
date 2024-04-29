05cc42bbf5e0cd0039bc5ec1203f79d2ce0993f6
iosmini <iosmini@info.com.np>
2024-04-29 10:25:46 +0545

    changes in main and pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:snkr_flutter/screen/login/login_screen.dart';
-import 'package:snkr_flutter/screen/splashScreen/splash_screen.dart';
-import 'package:flutter/widgets.dart';
 import 'package:snkr_flutter/Cart/CartPage.dart';
-import 'package:snkr_flutter/CustomerDashboard/Homepage.dart';
+import 'package:snkr_flutter/screen/splashScreen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: CartPage(),
       debugShowCheckedModeBanner: false,
     );
   }
